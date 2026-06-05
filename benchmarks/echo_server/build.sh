d2ab32602a64f2080e5299f53bf8e69ed0596647
desyang <desyang@qq.com>
2026-06-05 19:41:51 +0800

    fix: dynamic build need set LD_LIBRARY_PATH.

--- benchmarks/echo_server/build.sh
+++ benchmarks/echo_server/build.sh
@@ -1,1 +1,4 @@
 cmake -B build && cmake --build build -j4
+
+# 动态链接，若库未安装到系统目录，则需要设置链接地址
+# export LD_LIBRARY_PATH="/home/desyang/github/server-cpp/build/tcpserver-install/lib:$LD_LIBRARY_PATH"
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+# -DBUILD_SHARED_LIBS=ON 启用动态链接
 cmake -B build && cmake --build build -j4
